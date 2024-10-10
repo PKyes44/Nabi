19623db6bdd76ff56b127853bb29b4dc2e12b2e6
PKyes44 <rugate23@gmail.com>
2024-10-10 22:22:28 +0900

    chore: supabase client

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = "https://gxoibjaejbmathfpztjt.supabase.co";
+const URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-if (!KEY) throw new Error("key가 없음");
+if (!KEY) throw new Error("KEY가 없습니다");
+if (!URL) throw new Error("URL이 없습니다");
 
 export const supabase = createClient(URL, KEY);
