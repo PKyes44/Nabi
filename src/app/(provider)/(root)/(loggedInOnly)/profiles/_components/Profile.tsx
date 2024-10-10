08e080b1e5a555c26ff66252add64600be445b43
PKyes44 <rugate23@gmail.com>
2024-10-10 23:49:06 +0900

    feat: profile layout;

--- src/app/(provider)/(root)/(loggedInOnly)/profiles/_components/Profile.tsx
+++ src/app/(provider)/(root)/(loggedInOnly)/profiles/_components/Profile.tsx
@@ -1,17 +1,25 @@
 "use client";
 
+import clientApi from "@/api/clientSide/api";
 import { useAuthStore } from "@/zustand/auth.store";
 import { useQuery } from "@tanstack/react-query";
 
 function Profile() {
-  const currentUserId = useAuthStore((state) => state.currentUserId);
+  const userId = useAuthStore((state) => state.currentUserId);
 
-  useQuery({
-    queryKey: ["userProfiles", { userId: currentUserId }],
-    queryFn: () => {},
+  const { data: profile, isLoading } = useQuery({
+    queryKey: ["userProfiles", { userId }],
+    queryFn: () => clientApi.profiles.getProfileByUserId(userId!),
   });
 
-  return <div>Profile</div>;
+  if (isLoading) return <span>프로필 로딩 중 ..</span>;
+
+  return (
+    <div className="flex gap-x-7">
+      <article className="w-[900px] h-[600px] bg-gray-300 rounded-lg"></article>
+      <article className="grow h-96 bg-gray-300 rounded-lg"></article>
+    </div>
+  );
 }
 
 export default Profile;
--- src/app/(provider)/(root)/(loggedInOnly)/profiles/page.tsx
+++ src/app/(provider)/(root)/(loggedInOnly)/profiles/page.tsx
@@ -1,7 +1,12 @@
 import Page from "@/components/Page/Page";
+import Profile from "./_components/Profile";
 
 async function ProfilePage() {
-  return <Page width="lg">ProfilePage</Page>;
+  return (
+    <Page width="lg" className="mt-10 mb-10">
+      <Profile />
+    </Page>
+  );
 }
 
 export default ProfilePage;
