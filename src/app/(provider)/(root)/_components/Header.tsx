f4f4fffab33dac7a30e65492074e7405106000c0
심우섭 <sybiyong2@gmail.com>
2024-10-10 17:24:07 +0900

    refactor: 헤더 auth 네비게이션 컴포넌트화

--- src/app/(provider)/(root)/_components/Header.tsx
+++ src/app/(provider)/(root)/_components/Header.tsx
@@ -1,17 +1,8 @@
-"use client";
 import Page from "@/components/Page/Page";
-import { supabase } from "@/supabase/client";
-import { useAuthStore } from "@/zustand/auth.store";
 import Link from "next/link";
+import AuthNavigation from "./AuthNavigation";
 
 function Header() {
-  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
-  const authInitialized = useAuthStore((state) => state.authInitialized);
-
-  const handleClickLogOut = async () => {
-    await supabase.auth.signOut();
-  };
-
   return (
     <header className="h-16 shadow-lg">
       <Page
@@ -23,22 +14,7 @@ function Header() {
           나비
         </Link>
 
-        <nav>
-          {authInitialized ? (
-            isLoggedIn ? (
-              <button onClick={handleClickLogOut}>로그아웃</button>
-            ) : (
-              <ul className="flex gap-x-5">
-                <li>
-                  <Link href="/log-in">로그인</Link>
-                </li>
-                <li>
-                  <Link href="/sign-up">회원가입</Link>
-                </li>
-              </ul>
-            )
-          ) : null}
-        </nav>
+        <AuthNavigation />
       </Page>
     </header>
   );
