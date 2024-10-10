2cd490bb2f5b9597679081ca190ee725724852be
PKyes44 <rugate23@gmail.com>
2024-10-10 15:00:53 +0900

    feat: root layout

--- src/app/(provider)/(root)/layout.tsx
+++ src/app/(provider)/(root)/layout.tsx
@@ -1,5 +1,15 @@
-function RootLayout() {
-  return <div>RootLayout</div>;
+import { PropsWithChildren } from "react";
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
+
+function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
 }
 
 export default RootLayout;
