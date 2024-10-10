bf5cfd430e4c243d7b09cea146812b860396c216
PKyes44 <rugate23@gmail.com>
2024-10-10 14:57:54 +0900

    feat: header

--- src/app/(provider)/(root)/_components/Header.tsx
+++ src/app/(provider)/(root)/_components/Header.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Link href="/">나비</Link>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href="/log-in">로그인</Link>
+          </li>
+          <li>
+            <Link href="/sign-up">회원가입</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
