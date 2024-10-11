5bb816dcd52f7f55955f3fc4d753a661663f2860
PKyes44 <rugate23@gmail.com>
2024-10-11 13:38:09 +0900

    feat: my location at map

--- src/app/(provider)/(root)/sponsored-store/_components/KakaoMap.tsx
+++ src/app/(provider)/(root)/sponsored-store/_components/KakaoMap.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { useEffect } from "react";
+import useGeolocation from "./useGeolocation";
 
 declare global {
   interface Window {
@@ -6,18 +9,27 @@ declare global {
   }
 }
 
-const KakaoMap = () => {
+function KakaoMap() {
+  const location = useGeolocation();
+
   useEffect(() => {
     let container = document.getElementById(`map`); // 지도를 담을 영역의 DOM 레퍼런스
+    const center = location.loaded
+      ? new window.kakao.maps.LatLng(
+          location.coordinates!.lat,
+          location.coordinates!.lng
+        )
+      : new window.kakao.maps.LatLng(33.450701, 126.570667);
+    console.log(center, location);
     let options = {
-      center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도 중심 좌표
+      center, // 지도 중심 좌표
       level: 3, // 지도의 레벨(확대, 축소 정도)
     };
 
     new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
-  }, []);
+  }, [location]);
 
-  return <div id="map" style={{ width: "100vw", height: "100vh" }} />;
-};
+  return <div id="map" className="w-screen h-screen" />;
+}
 
 export default KakaoMap;
