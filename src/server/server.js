21fa0608a7cffacbddec8cec0dbc74ab27ed2467
PKyes44 <rugate23@gmail.com>
2024-10-13 11:36:58 +0900

    feat: monthly payment

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const cron = require("node-cron");
 const { createClient } = require("@supabase/supabase-js");
 const app = express();
 const port = 8080;
@@ -20,8 +21,6 @@ const apiSecretKey = "test_sk_zXLkKEypNArWmo50nX3lmeaxYG5R";
 const encryptedApiSecretKey =
   "Basic " + Buffer.from(apiSecretKey + ":").toString("base64");
 
-const billingKeyMap = new Map();
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -30,8 +29,6 @@ app.get("/", (req, res) => {
 app.post("/issue-billing-key", (req, res) => {
   const { customerKey, authKey, price, recipientId } = req.body;
 
-  // AuthKey 로 카드 빌링키 발급 API 를 호출하세요
-  // @docs https://docs.tosspayments.com/reference#authkey로-카드-빌링키-발급
   fetch(`https://api.tosspayments.com/v1/billing/authorizations/issue`, {
     method: "POST",
     headers: {
@@ -60,6 +57,7 @@ app.post("/issue-billing-key", (req, res) => {
       recipientId,
       sponsorId: customerKey,
       billingKey: result.billingKey,
+      isTermination: false,
     };
     const { error: insertError } = await supabase
       .from("sponsorShip")
@@ -74,6 +72,7 @@ app.post("/issue-billing-key", (req, res) => {
 
     if (selectError) throw new Error("supabase select error");
     console.log("userProfile: ", data);
+
     const orderId = crypto.randomUUID();
     const orderName = `나비: ${data.nickname}님의 정기후원`;
     const requestData = {
@@ -125,49 +124,71 @@ app.post("/issue-billing-key", (req, res) => {
   });
 });
 
-// 카드 자동결제 승인
-app.post("/confirm-billing", function (req, res) {
-  const {
-    customerKey,
-    amount,
-    orderId,
-    orderName,
-    customerEmail,
-    customerName,
-  } = req.body;
+cron.schedule("0 0 1 * *", function () {
+  console.log("cron actived");
+});
 
-  // 저장해두었던 빌링키로 카드 자동결제 승인 API 를 호출하세요.
-  fetch(
-    `https://api.tosspayments.com/v1/billing/${billingKeyMap.get(customerKey)}`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: encryptedApiSecretKey,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
+cron.schedule("* * * * *", () => {
+  (async function () {
+    const response = await supabase
+      .from("sponsorShip")
+      .select(
+        "sponsorShipId, sponsorId, price, billingKey, userProfiles!sponsorShip_sponsorId_fkey(nickname, email)"
+      )
+      .eq("isTermination", false);
+    console.log(response);
+    if (response.error) {
+      throw new Error(response.error);
+    }
+    console.log("sponsors: ", response.data);
+
+    response.data.forEach(async (data) => {
+      const orderId = crypto.randomUUID();
+      const sponsorShipId = data.sponsorShipId;
+      const customerKey = data.sponsorId;
+      const billingKey = data.billingKey;
+      const price = +data.price;
+      const orderName = `나비: ${data.userProfiles.nickname}님의 정기후원`;
+
+      const requestData = {
         customerKey,
-        amount,
+        amount: price,
         orderId,
+        orderName: orderName,
+        customerEmail: data.userProfiles.email,
+        customerName: data.userProfiles.nickname,
+      };
+
+      const paymentRes = await fetch(
+        `https://api.tosspayments.com/v1/billing/${billingKey}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: encryptedApiSecretKey,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestData),
+        }
+      );
+      const json = await paymentRes.json();
+
+      console.log("json: ", json);
+      const insertLogData = {
         orderName,
-        customerEmail,
-        customerName,
-      }),
-    }
-  ).then(async function (response) {
-    const result = await response.json();
-    console.log(result);
+        sponsorShipId,
+        amount: price,
+      };
+      console.log("insertLogData:", insertLogData);
+      const { error: insertLogError } = await supabase
+        .from("sponsorShipOrder")
+        .insert(insertLogData);
 
-    if (!response.ok) {
-      // TODO: 자동결제 승인 실패 비즈니스 로직을 구현하세요.
-      res.status(response.status).json(result);
-
-      return;
-    }
-
-    // TODO: 자동결제 승인 성공 비즈니스 로직을 구현하세요.
-    res.status(response.status).json(result);
-  });
+      if (insertLogError) {
+        console.log("insertLogError: ", insertLogError);
+        throw new Error("supabase log insert error");
+      }
+    });
+  })();
 });
 
 app.listen(port, () => console.log(`listen on http://localhost:${port}`));
