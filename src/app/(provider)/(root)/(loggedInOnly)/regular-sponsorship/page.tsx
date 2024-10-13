223ecd6a129f2cd718d866ecd564eb9d978ae347
심우섭 <sybiyong2@gmail.com>
2024-10-13 14:18:22 +0900

    debug: 변경사항 코드 삭제

--- src/app/(provider)/(root)/(loggedInOnly)/regular-sponsorship/page.tsx
+++ src/app/(provider)/(root)/(loggedInOnly)/regular-sponsorship/page.tsx
@@ -1,34 +1,24 @@
 "use client";
 
 import clientApi from "@/api/clientSide/api";
-<<<<<<< HEAD
-=======
 import ButtonGroup from "@/components/Button/ButtonGroup";
 import InputGroup from "@/components/Inputs/InputGroup";
 import Page from "@/components/Page/Page";
->>>>>>> develop
 import { useAuthStore } from "@/zustand/auth.store";
 import { useQuery } from "@tanstack/react-query";
 import {
   loadTossPayments,
   TossPaymentsPayment,
 } from "@tosspayments/tosspayments-sdk";
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-=======
 import { ComponentProps, useEffect, useState } from "react";
->>>>>>> develop
 
 const clientKey = process.env.NEXT_PUBLIC_TOSS_PAYMENT_CLIENT_KEY;
 if (!clientKey) throw new Error("cannot find toss client key");
 
 function RegularSponsorShipPage() {
   const [payment, setPayment] = useState<TossPaymentsPayment | null>(null);
-<<<<<<< HEAD
-=======
   const [price, setPrice] = useState("10000");
   const [priceErrorMsg, setPriceErrorMsg] = useState<string | null>(null);
->>>>>>> develop
 
   const userId = useAuthStore((state) => state.currentUserId);
 
@@ -42,11 +32,7 @@ function RegularSponsorShipPage() {
   });
 
   useEffect(() => {
-<<<<<<< HEAD
-    async function fetchPayment() {
-=======
     (async () => {
->>>>>>> develop
       try {
         if (!userId) return;
 
@@ -60,23 +46,11 @@ function RegularSponsorShipPage() {
       } catch (error) {
         console.error("Error fetching payment:", error);
       }
-<<<<<<< HEAD
-    }
-
-    fetchPayment();
-=======
     })();
->>>>>>> develop
   }, [clientKey, userId]);
 
   async function requestBillingAuth() {
     if (!payment || !user || !userProfile) return;
-<<<<<<< HEAD
-    await payment.requestBillingAuth({
-      method: "CARD",
-      successUrl: window.location.origin + "/payment/billing",
-      failUrl: window.location.origin + "/404",
-=======
     if (+price < 10000)
       return setPriceErrorMsg("최소 후원금액이 10,000₩ 입니다");
     await payment.requestBillingAuth({
@@ -85,23 +59,10 @@ function RegularSponsorShipPage() {
         window.location.origin +
         `/regular-sponsorship/billing/${"ff30dc7b-83f7-455b-b794-7ab5c1147d21"}/${price}`,
       failUrl: window.location.origin + "/regular-sponsorship/fail",
->>>>>>> develop
       customerEmail: user.email,
       customerName: userProfile.nickname,
     });
   }
-<<<<<<< HEAD
-
-  return (
-    <div className="wrapper">
-      <div className="box_section">
-        <h1>정기 결제</h1>
-        <button className="button" onClick={requestBillingAuth}>
-          정기 후원하기
-        </button>
-      </div>
-    </div>
-=======
   const handleChangePrice: ComponentProps<"input">["onChange"] = (e) => {
     const newPriceValue = e.target.value;
     setPrice(newPriceValue);
@@ -124,7 +85,6 @@ function RegularSponsorShipPage() {
         </div>
       </div>
     </Page>
->>>>>>> develop
   );
 }
 
