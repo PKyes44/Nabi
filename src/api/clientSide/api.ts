2e9f509f5b6ef32d3174796886cd0dbfa9d3d795
심우섭 <sybiyong2@gmail.com>
2024-10-14 11:34:36 +0900

    feat/sponsor_history_infinite_scroll

--- src/api/clientSide/api.ts
+++ src/api/clientSide/api.ts
@@ -2,8 +2,9 @@ import authAPI from "./auth.api";
 import freeMealsAPI from "./freeMeals.api";
 import profilesAPI from "./profiles.api";
 import recruitsAPI from "./recruits.api";
-import storageAPI from "./storage.api";
 import regularSponsorShipAPI from "./regularSponsorShip.api";
+import repliesAPI from "./replies.api";
+import storageAPI from "./storage.api";
 import storeDataAPI from "./storeData.api";
 import storeOwnersAPI from "./storeOwners.api";
 
@@ -16,6 +17,7 @@ const clientApi = {
   storeOwners: storeOwnersAPI,
   sponsorShip: regularSponsorShipAPI,
   freeMeal: freeMealsAPI,
+  reply: repliesAPI,
 };
 
 export default clientApi;
--- src/api/clientSide/recruits.api.ts
+++ src/api/clientSide/recruits.api.ts
@@ -41,11 +41,25 @@ const getSortedMyRecruits = async (userId: string) => {
   return response;
 };
 
+const getPaginatedRecruits = async (userId: string, page: number) => {
+  const response = await supabase
+    .from("recruits")
+    .select("*")
+    .eq("authorId", userId)
+    .order("createdAt", { ascending: false })
+    // 3개씩 보여주기
+    .range(page * 3, page * 3 + 2);
+  const recruits = response.data;
+
+  return recruits;
+};
+
 const recruitsAPI = {
   createRecruit,
   getRecruits,
   getSortedMyRecruits,
   getRecruit,
+  getPaginatedRecruits,
 };
 
 export default recruitsAPI;
