3780a933efc277725e65831e639d5338dbdc5520
심우섭 <sybiyong2@gmail.com>
2024-10-14 11:47:20 +0900

    chore: conflict debug test

--- src/api/clientSide/api.ts
+++ src/api/clientSide/api.ts
@@ -1,11 +1,9 @@
 import authAPI from "./auth.api";
-import chatsAPI from "./chats.api";
 import freeMealsAPI from "./freeMeals.api";
 import profilesAPI from "./profiles.api";
 import recruitsAPI from "./recruits.api";
 import regularSponsorShipAPI from "./regularSponsorShip.api";
 import repliesAPI from "./replies.api";
-import roomsAPI from "./rooms.api";
 import storageAPI from "./storage.api";
 import storeDataAPI from "./storeData.api";
 import storeOwnersAPI from "./storeOwners.api";
@@ -21,8 +19,6 @@ const clientApi = {
   freeMeal: freeMealsAPI,
   reply: repliesAPI,
   freeMeals: freeMealsAPI,
-  rooms: roomsAPI,
-  chats: chatsAPI,
 };
 
 export default clientApi;
