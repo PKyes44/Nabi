36ee66922405be1791e344b831a0fc384be97b23
PKyes44 <rugate23@gmail.com>
2024-10-15 04:30:40 +0900

    debug: https error - chat with external server

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -3,6 +3,6 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  "ws://port-0-nabi-backend-m29e62geab88e174.sel4.cloudtype.app/"
+  "https://port-0-nabi-backend-m29e62geab88e174.sel4.cloudtype.app/"
 );
 export default socket;
