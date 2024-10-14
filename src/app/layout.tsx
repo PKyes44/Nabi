502b1f558179614b65014c5cb8e574b7fc2fb4c1
PKyes44 <rugate23@gmail.com>
2024-10-15 04:59:35 +0900

    debug: kakao maps auto load false

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function HTMLLayout({
       <head>
         <Script
           type="text/javascript"
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services,clusterer,drawing`}
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services,clusterer,drawing&autoload=false`}
         />
       </head>
       <body className={"antialiased"}>{children}</body>
