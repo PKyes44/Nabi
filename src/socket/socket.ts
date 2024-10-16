95147f4a731a46598ba8dcbe489fc79fc87727e6
PKyes44 <rugate23@gmail.com>
2024-10-16 16:42:20 +0900

    socket

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -3,8 +3,7 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  // "https://port-0-nabi-backend-m29e62geab88e174.sel4.cloudtype.app"
-  "ws://localhost:8080"
+  "https://port-0-nabi-backend-m29e62geab88e174.sel4.cloudtype.app"
 );
 
 export default socket;
