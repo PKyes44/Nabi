e53ff5fe0c805619553ec6b71b00d27b4462a27b
심우섭 <sybiyong2@gmail.com>
2024-10-18 16:39:20 +0900

    refactor: review code all resolve

--- src/app/(provider)/(root)/profiles/_components/SideBar/RecipientList.tsx
+++ src/app/(provider)/(root)/profiles/_components/SideBar/RecipientList.tsx
@@ -2,28 +2,31 @@ import { Tables } from "@/supabase/database.types";
 import ApprovedUser from "./ApprovedUser";
 import NotApprovedUser from "./NotApprovedUser";
 
+type Recruit = Tables<"recruits"> & {
+  sponsorMeets: (Pick<Tables<"sponsorMeets">, "userId" | "status"> & {
+    userProfiles: Tables<"userProfiles"> | null;
+  })[];
+  recipientMeets: (Pick<Tables<"recipientMeets">, "userId" | "status"> & {
+    userProfiles: Tables<"userProfiles"> | null;
+  })[];
+};
+
 interface RecipientListProps {
-  recruit: Tables<"recruits"> & {
-    sponsorMeets: (Pick<Tables<"sponsorMeets">, "userId" | "status"> & {
-      userProfiles: Tables<"userProfiles"> | null;
-    })[];
-    recipientMeets: (Pick<Tables<"recipientMeets">, "userId" | "status"> & {
-      userProfiles: Tables<"userProfiles"> | null;
-    })[];
-  };
+  recruit: Recruit;
   profile: Tables<"userProfiles">;
 }
 
 function RecipientList({ recruit, profile }: RecipientListProps) {
+  function applicantCount(recruit: Recruit) {
+    return recruit.recipientMeets.filter((user) => user.status === "approved")
+      .length;
+  }
+
   return (
     <section>
       <strong className="font-medium">
         신청한 아동 목록(
-        {
-          recruit.recipientMeets.filter((user) => user.status === "approved")
-            .length
-        }
-        /{recruit.maxRecipientRecruits})
+        {applicantCount(recruit)}/{recruit.maxRecipientRecruits})
       </strong>
 
       <ul className="mt-2 flex flex-col gap-y-3">
@@ -31,9 +34,7 @@ function RecipientList({ recruit, profile }: RecipientListProps) {
         <ApprovedUser meets={recruit.recipientMeets} />
 
         {/* 인원이 다 차지 않았으면 신청자 보여주기 */}
-        {recruit.maxRecipientRecruits >
-          recruit.recipientMeets.filter((user) => user.status === "approved")
-            .length && (
+        {recruit.maxRecipientRecruits > applicantCount(recruit) && (
           <NotApprovedUser
             meets={recruit.recipientMeets}
             profile={profile}
