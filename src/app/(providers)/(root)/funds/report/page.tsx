004f64bd4e050510d8de2d18c52a39b570e8a276
심우섭 <sybiyong2@gmail.com>
2024-10-23 15:51:06 +0900

    style: business section

--- src/app/(providers)/(root)/funds/report/page.tsx
+++ src/app/(providers)/(root)/funds/report/page.tsx
@@ -1,10 +1,12 @@
 import Container from "@/components/Container/Container";
+import Business from "./_components/Business";
 import Finances from "./_components/Finances";
 
 function FundReportPage() {
   return (
     <Container width="xs" isMain={false} className="pt-16">
       <Finances />
+      <Business />
     </Container>
   );
 }
