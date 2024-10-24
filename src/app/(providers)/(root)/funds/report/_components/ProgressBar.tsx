7e4eaf097e610b54c26c724174c194b14b05d079
PKyes44 <rugate23@gmail.com>
2024-10-24 13:01:32 +0900

    report header navigation

--- src/app/(providers)/(root)/funds/report/_components/ProgressBar.tsx
+++ src/app/(providers)/(root)/funds/report/_components/ProgressBar.tsx
@@ -1,54 +1,12 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 interface ProgressBarProps {
-  elementId: string;
+  isShown: boolean;
+  theme: number;
+  translateXValue: number;
 }
 
-function ProgressBar({ elementId }: ProgressBarProps) {
-  const [translateValue, setTranslateValue] = useState(460);
-  const [isShown, setIsShown] = useState(false);
-  const [theme, setTheme] = useState(1);
-
-  const onScroll = (e: Event) => {
-    const { scrollTop, scrollHeight } = e.target as unknown as {
-      scrollTop: number;
-      scrollHeight: number;
-    };
-
-    const firstSection = scrollHeight / 2 - 30;
-    let translateXValue = 0;
-
-    if (firstSection <= scrollTop) {
-      translateXValue =
-        460 -
-        Math.floor(((scrollTop - firstSection) / (firstSection - 695)) * 460) +
-        15;
-      setTheme(2);
-    } else {
-      translateXValue = 460 - Math.floor((scrollTop / firstSection) * 460);
-      setTheme(1);
-    }
-
-    setTranslateValue(translateXValue);
-    setIsShown(true);
-  };
-
-  useEffect(() => {
-    console.log("elementId ", elementId);
-    if (!elementId) return;
-    const reportElment = document.getElementById(elementId);
-    if (!reportElment) return;
-
-    reportElment.addEventListener("scroll", onScroll);
-
-    return () => {
-      setIsShown(false);
-      reportElment.removeEventListener("scroll", onScroll);
-    };
-  }, []);
-
+function ProgressBar({ isShown, theme, translateXValue }: ProgressBarProps) {
   return (
     isShown && (
       <div
@@ -57,7 +15,7 @@ function ProgressBar({ elementId }: ProgressBarProps) {
         }`}
       >
         <div
-          style={{ transform: `translateX(-${translateValue}px)` }}
+          style={{ transform: `translateX(-${translateXValue}px)` }}
           className={`h-4 w-[460px] ${
             theme === 1 ? "bg-orange-500" : "bg-sky-500"
           }`}
