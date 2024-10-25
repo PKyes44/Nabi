145335e7c37414ac89c6d50b4b49d8d0cdd95a8e
심우섭 <sybiyong2@gmail.com>
2024-10-25 14:10:07 +0900

    style: header skeleton

--- src/app/(providers)/(root)/_components/Headers/Header/Navigation/Navigation.tsx
+++ src/app/(providers)/(root)/_components/Headers/Header/Navigation/Navigation.tsx
@@ -30,7 +30,12 @@ function Navigation() {
               </li>
             </>
           )
-        ) : null}
+        ) : (
+          <div className="flex gap-x-5 items-center justify-center">
+            <div className="w-10 h-10 rounded-2xl bg-gray-200"></div>
+            <div className="w-10 h-10 rounded-2xl bg-gray-200" />
+          </div>
+        )}
       </ul>
     </nav>
   );
