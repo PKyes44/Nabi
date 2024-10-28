bfb79ed26b725bef895e8abbb9ae590e4a1f28cc
PKyes44 <rugate23@gmail.com>
2024-10-28 16:21:14 +0900

    show remaining dead line date

--- src/app/(providers)/(root)/(home)/_components/Recruits/RecruitDetails.tsx
+++ src/app/(providers)/(root)/(home)/_components/Recruits/RecruitDetails.tsx
@@ -8,9 +8,7 @@ import "dayjs/locale/ko";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 import ApplyButtons from "./ApplyButtons";
-import DeadLineToast from "./DeadLineToast";
 import OthersButton from "./OthersButton";
 import RecruitCount from "./RecruitCount";
 
@@ -22,24 +20,14 @@ interface RecruitDetailsProps {
 }
 
 function RecruitDetails({ recruit }: RecruitDetailsProps) {
-  const [isHover, setIsHover] = useState(false);
   const isAuthInitialized = useAuthStore((state) => state.isAuthInitialized);
   const currentUser = useAuthStore((state) => state.currentUser);
   const createdAt = dayjs(recruit.createdAt).fromNow();
-
-  const handleActiveToast = () => {
-    setIsHover(true);
-  };
-  const handleInactiveToast = () => {
-    setIsHover(false);
-  };
+  const isPassedDeadLineDate = dayjs().isBefore(recruit.deadLineDate);
+  const remainDeadLineDate = dayjs(recruit.deadLineDate).toNow();
 
   return (
-    <section
-      onMouseOver={handleActiveToast}
-      onMouseOut={handleInactiveToast}
-      className="p-8 px-6"
-    >
+    <section className="p-8 px-6">
       <div className="flex items-center justify-between">
         <Link
           href={`/profiles?userId=${recruit.authorId}`}
@@ -75,10 +63,6 @@ function RecruitDetails({ recruit }: RecruitDetailsProps) {
         <ApplyButtons recruit={recruit} />
       )}
 
-      <DeadLineToast
-        deadLineDate={dayjs(recruit.deadLineDate).format("YYYY년 MM월 DD일")}
-        isHover={isHover}
-      />
       <article className="flex flex-col gap-y-4 mt-3">
         {/* 제목 */}
         <h2 className="font-bold text-lg">{recruit.title}</h2>
@@ -87,39 +71,46 @@ function RecruitDetails({ recruit }: RecruitDetailsProps) {
         <p className="font-normal text-sm leading-6">{recruit.content}</p>
 
         {/* 장소, 일시 등 */}
-        <div className="flex gap-x-4">
-          <div className="flex gap-x-2 items-center group relative">
-            <Image
-              width={150}
-              height={150}
-              className="w-4 aspect-square"
-              src="https://gxoibjaejbmathfpztjt.supabase.co/storage/v1/object/public/icons/Location.png?t=2024-10-15T19%3A39%3A08.745Z"
-              alt="location icon"
-            />
-            <span className="font-light text-xs">{recruit.region}</span>
-            <span className="whitespace-nowrap absolute top-6 left-3 font-normal text-xs invisible group-hover:visible">
-              집합 장소
-            </span>
+        <section className="flex justify-between">
+          <div className="flex gap-x-4">
+            <div className="flex gap-x-2 items-center group relative">
+              <Image
+                width={150}
+                height={150}
+                className="w-4 aspect-square"
+                src="https://gxoibjaejbmathfpztjt.supabase.co/storage/v1/object/public/icons/Location.png?t=2024-10-15T19%3A39%3A08.745Z"
+                alt="location icon"
+              />
+              <span className="font-light text-xs">{recruit.region}</span>
+              <span className="whitespace-nowrap absolute top-6 left-3 font-normal text-xs invisible group-hover:visible">
+                집합 장소
+              </span>
+            </div>
+            <div className="flex gap-x-2 items-center group relative">
+              <Image
+                width={150}
+                height={150}
+                className="w-4 aspect-square"
+                src="https://gxoibjaejbmathfpztjt.supabase.co/storage/v1/object/public/icons/Date.png"
+                alt="date icon"
+              />
+              <span className="font-light text-xs">
+                {dayjs(recruit.volunteeringDate)
+                  .locale("ko")
+                  .format("YYYY-MM-DD (ddd) HH:mm")}
+              </span>
+              <span className="whitespace-nowrap absolute top-6 left-1/3 font-normal text-xs invisible group-hover:visible">
+                봉사활동 일시
+              </span>
+            </div>
+            <RecruitCount recruit={recruit} />
           </div>
-          <div className="flex gap-x-2 items-center group relative">
-            <Image
-              width={150}
-              height={150}
-              className="w-4 aspect-square"
-              src="https://gxoibjaejbmathfpztjt.supabase.co/storage/v1/object/public/icons/Date.png"
-              alt="date icon"
-            />
-            <span className="font-light text-xs">
-              {dayjs(recruit.volunteeringDate)
-                .locale("ko")
-                .format("YYYY-MM-DD (ddd) HH:mm")}
+          {!isPassedDeadLineDate && (
+            <span className="text-red-400 text-xs font-semibold">
+              {remainDeadLineDate} 마감됩니다
             </span>
-            <span className="whitespace-nowrap absolute top-6 left-1/3 font-normal text-xs invisible group-hover:visible">
-              봉사활동 일시
-            </span>
-          </div>
-          <RecruitCount recruit={recruit} />
-        </div>
+          )}
+        </section>
       </article>
     </section>
   );
