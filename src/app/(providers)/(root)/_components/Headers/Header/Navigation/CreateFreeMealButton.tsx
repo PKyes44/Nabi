3651f4da82ddb46362ecc07bb32231dce19cd3cc
심우섭 <sybiyong2@gmail.com>
2024-10-28 10:50:09 +0900

    refactor: free meal button jsx code refactory

--- src/app/(providers)/(root)/_components/Headers/Header/Navigation/CreateFreeMealButton.tsx
+++ src/app/(providers)/(root)/_components/Headers/Header/Navigation/CreateFreeMealButton.tsx
@@ -18,19 +18,21 @@ function CreateFreeMealButton({ userId }: CreateFreeMealButtonProps) {
     setActiveModal(<CreateFreeMealModal />);
   };
 
-  return isStoreOwner ? (
-    <li className="w-10 h-10">
-      <button onClick={handleClickCreateFreeMeal}>
-        <Image
-          width={150}
-          height={150}
-          className="w-10 aspect-square rounded-lg"
-          src="https://gxoibjaejbmathfpztjt.supabase.co/storage/v1/object/public/icons/LinkToFreeMeal%20.png?t=2024-10-15T21%3A07%3A35.956Z"
-          alt="create free-meal post icon"
-        />
-      </button>
-    </li>
-  ) : null;
+  return (
+    isStoreOwner && (
+      <li className="w-10 h-10">
+        <button onClick={handleClickCreateFreeMeal}>
+          <Image
+            width={150}
+            height={150}
+            className="w-10 aspect-square rounded-lg"
+            src="https://gxoibjaejbmathfpztjt.supabase.co/storage/v1/object/public/icons/LinkToFreeMeal%20.png?t=2024-10-15T21%3A07%3A35.956Z"
+            alt="create free-meal post icon"
+          />
+        </button>
+      </li>
+    )
+  );
 }
 
 export default CreateFreeMealButton;
