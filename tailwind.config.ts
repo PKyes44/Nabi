995bab70b745e83073c48cd7feccfeaa0376f64d
심우섭 <sybiyong2@gmail.com>
2024-10-29 13:40:44 +0900

    debug:report page animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  mode: "jit",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -58,5 +59,13 @@ const config: Config = {
     },
   },
   plugins: [],
+  safelist: [
+    "animate-titleShow",
+    "animate-leftHandShow",
+    "animate-rightHandShow",
+    "animate-titleHidden",
+    "animate-leftHandHidden",
+    "animate-rightHandHidden",
+  ],
 };
 export default config;
