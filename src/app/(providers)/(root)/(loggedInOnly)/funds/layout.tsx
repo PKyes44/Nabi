9aec5b9d9982a78d7e059bd9bb15f9521cd09018
PKyes44 <rugate23@gmail.com>
2024-10-30 10:48:02 +0900

    refactor: fund page error toast

--- src/app/(providers)/(root)/(loggedInOnly)/funds/layout.tsx
+++ src/app/(providers)/(root)/(loggedInOnly)/funds/layout.tsx
@@ -1,14 +1,23 @@
 "use client";
 
+import { ToastType } from "@/types/toast.types";
 import { useAuthStore } from "@/zustand/auth.store";
+import { useToastStore } from "@/zustand/toast.store";
 import { useRouter } from "next/navigation";
 import { PropsWithChildren } from "react";
 
 function FundLayout({ children }: PropsWithChildren) {
   const user = useAuthStore((state) => state.currentUser);
+  const addToast = useToastStore((state) => state.addToast);
   const router = useRouter();
   if (user?.role !== "sponsor") {
-    alert("후원하기 페이지는 후원자만 이용 가능합니다.");
+    const toast: ToastType = {
+      id: crypto.randomUUID(),
+      title: "권한 부족",
+      content: "후원자만 이용 가능한 서비스입니다",
+      type: "fail",
+    };
+    addToast(toast);
     router.replace("/");
   }
 
