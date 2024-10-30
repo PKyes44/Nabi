862c1e774186e0d09288e1d0f1e7c4098f798400
심우섭 <sybiyong2@gmail.com>
2024-10-30 17:34:00 +0900

    refactor: navigation responsive ui

---
+++ src/app/(providers)/(root)/_components/Headers/Header/HeaderNavigation/Navigation.tsx
@@ -0,0 +1,13 @@
+import HeaderNavigationLink from "./HeaderNavigationLink";
+
+function Navigation() {
+  return (
+    <nav className="flex items-center gap-x-5">
+      <HeaderNavigationLink href="/funds" label="후원기금 모금하기" />
+      <HeaderNavigationLink href="/funds/report" label="후원 리포트" />
+      <HeaderNavigationLink href="/free-meals/map" label="아동급식카드 지도" />
+    </nav>
+  );
+}
+
+export default Navigation;
