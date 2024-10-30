ab2c6ad0dd1d32712a91e48bd661ddcc8d7ce61e
joeunjin1369 <joeunjin1369@gmail.com>
2024-10-30 17:31:25 +0900

    로그인, 회원가입 페이지 반응형 디자인

--- src/app/(providers)/(root)/(auth)/log-in/page.tsx
+++ src/app/(providers)/(root)/(auth)/log-in/page.tsx
@@ -10,25 +10,32 @@ function LogInPage() {
 
   return (
     <Container width="sm" className="flex flex-col items-center">
-      <h1 className="mt-32 mb-10 text-3xl font-bold">로그인 하기</h1>
+      <h1 className="mt-32 mb-10 text-3xl font-bold sm:text-2xl sm:mt-20">
+        로그인 하기
+      </h1>
 
-      <form onSubmit={handleSubmitLogInForm} className="flex flex-col gap-y-3">
+      <form
+        onSubmit={handleSubmitLogInForm}
+        className="flex flex-col gap-y-3 sm:text-xs w-96 md:w-80 sm:w-72"
+      >
         <InputGroup
           type="email"
           errorText={errMsgs.email}
+          wrapperClassName="w-full"
           label="이메일"
           name="email"
         />
         <InputGroup
           type="password"
           errorText={errMsgs.password}
+          wrapperClassName="w-full"
           label="비밀번호"
           name="password"
         />
 
         <ButtonGroup
           errorText={errMsgs.global}
-          className="w-full mt-5"
+          className="w-full mt-5 sm:text-xs"
           size="md"
           value="로그인"
         />
--- src/app/(providers)/(root)/(auth)/sign-up/page.tsx
+++ src/app/(providers)/(root)/(auth)/sign-up/page.tsx
@@ -16,34 +16,44 @@ function SignUpPage({ searchParams: { role } }: SignUpPageProps) {
 
   return (
     <Container width="md" className="flex flex-col items-center">
-      <h1 className="mt-20 mb-2 text-3xl font-bold">회원가입</h1>
-      <span className="mb-10 text-sm">
+      <h1 className="mt-20 mb-2 text-3xl font-bold sm:text-2xl">회원가입</h1>
+      <span className="mb-10 text-sm text-center sm:text-xs">
         {role === "sponsor"
           ? "후원자로서 경제적으로 어려운 아이들을 도와보세요!"
           : "다양한 이유로 어려움을 가지고 있다면 나비에 가입하여 도움을 받아보세요!"}
       </span>
 
-      <form onSubmit={handleSubmitSignUpForm} className="flex flex-col gap-y-2">
+      <form
+        onSubmit={handleSubmitSignUpForm}
+        className="flex flex-col gap-y-2 sm:text-xs w-96 md:w-80 sm:w-72"
+      >
         <InputGroup
           type="email"
+          wrapperClassName="w-full"
           errorText={errMsgs.email}
           label="이메일"
           name="email"
         />
         <InputGroup
           type="password"
+          wrapperClassName="w-full"
           errorText={errMsgs.password}
           label="비밀번호"
           name="password"
         />
         <InputGroup
           type="text"
+          wrapperClassName="w-full"
           errorText={errMsgs.nickname}
           label="닉네임"
           name="nickname"
         />
 
-        <ButtonGroup value="회원가입" size="md" className="w-full mt-5" />
+        <ButtonGroup
+          value="회원가입"
+          size="md"
+          className="w-full mt-5 sm:text-xs"
+        />
       </form>
     </Container>
   );
